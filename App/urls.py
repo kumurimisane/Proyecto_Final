95e487a807f27ad23d340979323bf4faf8862c95
Nacho Fernandez <igmyweb7@gmail.com>
2023-04-29 19:40:26 -0300

    primera carga

--- App/urls.py
+++ App/urls.py
@@ -1,14 +1,27 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from .views import *
 
 urlpatterns = [
     path('', inicio, name='Inicio'),
     path('cartas/', cartas, name='Cartas'),
+    path('crear-cartas/', crearCartas, name='crearCartas'),
+    path('eliminar-cartas/<int:id>', eliminarCartas, name='eliminarCartas'),
+    path('edita-cartas/<int:id>', editarCartas, name='editarCartas'),
     path('torneo/', torneo, name='torneo'),
     path('crear-torneo/', crearTorneo, name='crearTorneo'),
     path('eliminar-torneo/<int:id>', eliminarTorneo, name='eliminarTorneo'),
     path('edita-torneo/<int:id>', editarTorneo, name='editarTorneo'),
+    path('mazos/', MazoList.as_view(), name='Mazos'),
+    path('mazos-detail/', MazoDetail.as_view(), name='mazoDeteail'),
+    path('mazos-create/', MazoCreate.as_view(), name='crearMazo'),
+    path('mazos-update/<int:pk>', MazoUpdate.as_view(), name='editarMazo'),
+    path('mazos-delete/<int:pk>', MazoDelete.as_view(), name='eliminarMazo'),
     path('contactenos/', contactenos, name='Contactenos'),
     path('about/', about, name='About'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- App/views.py
+++ App/views.py
@@ -3,13 +3,91 @@ from django.urls import reverse
 from .models import *
 from .forms import *
 from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
 # Create your views here.
 
 def inicio(request):
     return render(request, 'index.html')
 
+#CRUD Cartas
+
+#Read Cartas
 def cartas(request):
-    return render(request, 'cartas.html')
+
+    miscartas = Cartas.objects.all()   
+
+    return render(request, 'cartas.html', {"miscartas": miscartas})
+
+
+# Create Cartas
+def crearCartas(request):
+    if request.method == 'POST':
+        miFormulario = CartasFormulario(request.POST, request.FILES)
+        if miFormulario.is_valid():
+            dataformulario = miFormulario.cleaned_data
+            inscripcion = Cartas(nombre = dataformulario['nombre'],
+                                 tipo = dataformulario['tipo'], 
+                                 nivel = dataformulario['nivel'], 
+                                 ataque = dataformulario['ataque'], 
+                                 defensa = dataformulario['defensa'], 
+                                 descripcion = dataformulario['descripcion'],
+                                 imagen = dataformulario['imagen']
+                                 )
+            inscripcion.save()
+            return redirect(reverse('Inicio'))
+        else:
+            return render(request, 'cartas.html',{"mensaje":"Formulario Invalido"})
+
+    else:
+        miFormulario = CartasFormulario()
+        return render(request, 'crear-cartas.html', {'miFormulario': miFormulario})
+    
+#Delete Cartas
+
+def eliminarCartas(request, id):
+    if request.method == 'POST':
+
+        cartas = Cartas.objects.get(id=id)
+        cartas.delete()
+
+        cartas = Cartas.objects.all()
+        return render(request, 'eliminarcartas.html', {"cartas":cartas} )
+
+#Update Cartas
+
+def editarCartas(request, id):
+
+    cartas = Cartas.objects.get(id=id)
+    
+    if request.method == 'POST':
+        miFormulario = CartasFormulario(request.POST)
+        if miFormulario.is_valid():
+            dataformulario = miFormulario.cleaned_data
+            cartas.nombre = dataformulario['nombre']
+            cartas.tipo = dataformulario['tipo']
+            cartas.nivel = dataformulario['nivel']
+            cartas.ataque = dataformulario['ataque']
+            cartas.defensa = dataformulario['defensa']
+            cartas.descripcion = dataformulario['descripcion']
+            cartas.imagen = dataformulario['imagen']
+            cartas.save()
+            return redirect(reverse('Inicio'))
+        else:
+            return render(request, 'cartas.html',{"mensaje":"Formulario Invalido"})
+
+    else:
+        miFormulario = CartasFormulario(initial={
+            "nombre":cartas.nombre,
+            "tipo":cartas.tipo,
+            "nivel":cartas.nivel,
+            "ataque": cartas.ataque,    
+            "defensa":cartas.defensa,
+            "descripcion":cartas.descripcion,
+            "Imagen": cartas.imagen
+        })
+        return render(request, 'editar-torneo.html', {'miFormulario': miFormulario})
 
 
 #CRUD Torneo
@@ -87,6 +165,35 @@ def editarTorneo(request, id):
         })
         return render(request, 'editar-torneo.html', {'miFormulario': miFormulario})
 
+class MazoList(ListView):
+    
+    model = Mazo
+    template_name= "mazos.html"
+    context_object_name = 'mazos'
+
+class MazoDetail(DetailView):
+    
+    model = Mazo
+    template_name = "mazos_detail.html"
+    context_object_name = 'mazodetail'
+
+class MazoCreate(CreateView):
+    model = Mazo
+    template_name = "mazo_create.html"
+    fields = ['nombre','tipo','carta_principal','cantidad_de_cartas', 'descripcion', 'imagen']
+    success_url = '/App/'
+
+class MazoUpdate(UpdateView):
+    model = Mazo
+    template_name = "mazo_update.html"
+    fields = ('__all__')
+    context_object_name = 'mazoupdate'
+    success_url = '/App/'
+
+class MazoDelete(DeleteView):
+    model = Mazo
+    template_name = "mazo_delete.html"
+    success_url = '/App/'
 
 def contactenos(request):
     return render(request, 'contactenos.html')
